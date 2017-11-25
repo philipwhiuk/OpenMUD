591185947de99d14b364221d37ae755ce02c2fac
Philip Whitehouse <philip@whiuk.com>
2017-11-25 01:55:00 +0000

    Add message constants

---
+++ src/main/java/com/whiuk/philip/openmud/Messages.java
@@ -0,0 +1,16 @@
+package com.whiuk.philip.openmud;
+
+public final class Messages {
+	public static final class ToServer {
+		public static final byte LOGIN = 1;
+		public static final byte LOGOUT = 2;
+		public static final byte MOVE = 3;
+	}
+	public static final class FromServer {
+		public static final byte LOGIN_SUCCESS = 1;
+		public static final byte LOGIN_FAILURE = 2;
+		public static final byte LOGGED_OUT = 3;
+		public static final byte ROOM = 3;
+		public static final byte TEXT = 4;
+	}
+}
